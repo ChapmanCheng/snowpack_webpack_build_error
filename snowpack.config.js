fe77dfd421512fef761491ed7ac3f4f23f3ef8e7
chapmancheng <chapboy1989@gmail.com>
2021-05-23 20:09:42 +0800

    initial commit

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,10 +1,26 @@
+const fs = require('fs')
+const path = require('path')
+
 /** @type {import("snowpack").SnowpackUserConfig } */
 module.exports = {
   mount: {
     public: { url: '/', static: true },
     src: { url: '/dist' },
   },
-  plugins: ['@snowpack/plugin-react-refresh', '@snowpack/plugin-dotenv'],
+  plugins: [
+    '@snowpack/plugin-react-refresh',
+    '@snowpack/plugin-dotenv',
+    [
+      '@snowpack/plugin-webpack',
+      { 
+        manifest: true,
+        // extendConfig: (config) => {
+          
+        //   return config
+        // }
+      },
+    ],
+  ],
   routes: [
     /* Enable an SPA Fallback in development: */
     // {"match": "routes", "src": ".*", "dest": "/index.html"},
--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
-import './index.css';
+
+import './styles.css'
 
 ReactDOM.render(
   <React.StrictMode>
@@ -9,9 +10,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root'),
 );
-
-// Hot Module Replacement (HMR) - Remove this snippet to remove HMR.
-// Learn more: https://www.snowpack.dev/concepts/hot-module-replacement
-if (import.meta.hot) {
-  import.meta.hot.accept();
-}
